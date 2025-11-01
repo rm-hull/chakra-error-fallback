6f2bf62b727fc8f3d2e04d6ebb1c6c81e9817618
Richard Hull <rm_hull@yahoo.co.uk>
2025-11-01 18:54:49 +0000

    chore: Initialize project structure and dev tooling
    Set up essential development tooling and infrastructure for the
    new package.
    
    *   Configure CI/CD workflows using GitHub Actions.
    *   Implement automatic versioning and release management via
        `changesets`.
    *   Set up Yarn 4, modern flat ESLint config (TS/React), Vitest
        for testing (with JSDOM and coverage), and Tsup for building.
    *   Add configuration for Dependabot.

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: ["dist"],
+  },
+  {
+    files: ["**/*.{ts,tsx}"],
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+    languageOptions: {
+      ecmaVersion: "latest",
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": "warn",
+    }
+  },
+);
