1ac96c8425bf9f380c442135f9e8eb57a45f7491
Richard Hull <rm_hull@yahoo.co.uk>
2026-03-13 18:03:55 +0000

    tsconfig fix + yarn version bump

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   base: "/chakra-error-fallback",
   test: {
     globals: true,
@@ -20,4 +19,5 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
+  resolve: { tsconfigPaths: true }
 });
